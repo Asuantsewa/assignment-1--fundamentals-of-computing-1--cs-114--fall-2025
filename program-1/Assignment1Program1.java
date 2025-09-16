8de21a87381c2073d4152b3a6c28c089328a7a6e
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-09-16 11:25:44 -0400

    Sample program(first draft)

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -3,3 +3,14 @@ public class Assignment1Program1 {
 
   }
 }
+// letter1 and letter2 would be created by some other method that you must define
+String[] letter1 = new String[]{"JJJJJJJJJJ", "JJ  JJ  JJ", "JJ  JJ  JJ", "JJ JJ  JJ", "JJ
+JJ  JJ", "JJ      JJ", "JJ      JJ"};
+String[] letter2 = new String[]{"EEEEEEEEE" , "EEE", "EE       ", "EE       ", "EE       ","EEEEE", "EEEEEEEEE"};
+
+String[] concat = new String[letter1.length];
+
+for (int i = 0 ; i < concat.length ; i++)
+    concat[i] = letter1[i] + "\t" + letter2[i];
+for (String s : concat)
+    System.out.println(s);
