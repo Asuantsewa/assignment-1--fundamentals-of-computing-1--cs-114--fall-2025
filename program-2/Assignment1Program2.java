aacc4059ccec5644b03929316e050485b638ed69
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-09-18 14:00:03 -0400

    changed identation

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -12,7 +12,7 @@ public class Assignment1Program2 {
 
 // 1. Get the base-10 number and the target base from the user
   System.out.print("Enter a base-10 number to convert: ");
-  int base10Number = scanner.nextInt();
+    int base10Number = scanner.nextInt();
 
   System.out.print("Enter the target base (between 2 and 36): ");
   int targetBase = scanner.nextInt();
@@ -25,11 +25,11 @@ public class Assignment1Program2 {
 
 // 2. Convert the base-10 number to the target base
   String convertedNumber = convertToBase(base10Number, targetBase);
-  System.out.println("Converted number: " + convertedNumber);
+    System.out.println("Converted number: " + convertedNumber);
 
 // 3. Calculate the largest 4-digit number in the given base
   int largest4DigitNumber = (int) Math.pow(targetBase, 4) - 1;
-  System.out.println("Largest 4-digit number in base " + targetBase + ": " + largest4DigitNumber);
+    System.out.println("Largest 4-digit number in base " + targetBase + ": " + largest4DigitNumber);
 }
 
 // Method to convert a base-10 number to a different base (2 to 36)
